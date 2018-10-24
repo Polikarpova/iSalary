6060974277843f3e5a2a546401bd0772b6d2e88d
DimaDontsov <Dimayahoo@mail.ru>
2018-10-24 12:26:32 +0300

    Создан класс Продукт (Product)

--- iSalary/iSalary/Product.cpp
+++ iSalary/iSalary/Product.cpp
@@ -9,3 +9,27 @@ Product::Product(void)
 Product::~Product(void)
 {
 }
+
+void Product::setId( int _id ){
+	id = _id;
+}
+
+int Product::getId() {
+	return id;
+}
+
+void Product::setName( QString _name) {
+	name = _name;
+}
+
+QString Product::getName() {
+	return name;
+}
+
+void Product::setCommission( double _commission) {
+	commission = _commission;
+}
+
+double Product::getCommission() {
+	return commission;
+}
--- iSalary/iSalary/Product.h
+++ iSalary/iSalary/Product.h
@@ -4,9 +4,19 @@ class Product {
 private:
 	QString name;
 	double commission;
+	int id;
 
 public:
 	Product(void);
 	~Product(void);
+
+	void setId( int _id );
+	int getId();
+
+	void setName( QString _name );
+	QString getName();
+
+	void setCommission( double _commission );
+	double getCommission();
 };
 
