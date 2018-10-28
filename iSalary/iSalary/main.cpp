3a4d339de7e8d3bc3befa2d6741fc466ccbc231f
Kurnosov Andrew <dryush97@gmail.com>
2018-10-28 12:47:13 +0300

    Инициализация основных компонентов в main

--- iSalary/iSalary/main.cpp
+++ iSalary/iSalary/main.cpp
@@ -1,10 +1,38 @@
-#include "mainwindow.h"
 #include <QtWidgets/QApplication>
+
+#include "mainwindow.h"
+#include "AuthorizationFacade.h"
+#include "UserDB.h"
+#include "AuthPage.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	MainWindow w;
+
+    QSqlDatabase sqlDB;
+    
+	sqlDB= QSqlDatabase::addDatabase( "QMYSQL");
+	sqlDB.setHostName( "127.0.0.1");
+    sqlDB.setPort( 3306);
+    sqlDB.setDatabaseName( "test");
+    sqlDB.setUserName( "root");
+    sqlDB.setPassword( "root");
+    bool isOpen = sqlDB.open();
+
+    UserDB * userDB = new UserDB( &sqlDB);
+    AuthorizationModule * authModule = new AuthorizationModule( userDB);
+    AuthorizationFacade * authFacade = new AuthorizationFacade( authModule);
+    AuthPage * authPage = new AuthPage( authFacade);
+
+    MainWindow w( authPage);
 	w.show();
-	return a.exec();
+
+	int exitCode = a.exec();
+
+    delete authPage;
+    delete authFacade;
+    delete authModule;
+    delete userDB;
+
+    return exitCode;
 }
