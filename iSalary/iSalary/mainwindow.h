b7226f77ed6fefb211de0de69a12a2354ea354cb
Kurnosov Andrew <dryush97@gmail.com>
2018-10-28 12:46:40 +0300

    Реализован переход к окнам Менеджера и Руководителя после авторизации

--- iSalary/iSalary/mainwindow.h
+++ iSalary/iSalary/mainwindow.h
@@ -4,18 +4,40 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_mainwindow.h"
 
+#include "AuthPage.h"
+
+/**
+* Перечисление основных виджетов программы
+*/
+enum ProgramWidgets {
+    AUTH_WIDGET = 0,
+    PROGRAM_WIDGET = 1
+};
+
+/**
+* Перечисление основных виджетов по ролям
+*/
+enum UserRoleWidgets {
+    BOSS_WIDGET = 0,
+    MANAGER_WIDGET = 1
+};
+
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
 public:
-	MainWindow(QWidget *parent = 0);
+	MainWindow(AuthPage* authPage, QWidget *parent = 0);
 	~MainWindow();
 
 private:
 	Ui::MainWindowClass ui;
 
+    AuthPage* authPage;
+
 	void createHorizontalTabs();
+    void enterProgram( const UserDTO& user, UserType userType);
+    
 };
 
 #endif // MAINWINDOW_H
