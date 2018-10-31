c7680bc64a23fc2f1e4a606711f940027d2751ed
DimaDontsov <Dimayahoo@mail.ru>
2018-10-31 14:08:30 +0400

    Изменён класс Sale_DB
    Изменения:
    - теперь create принимает ActiveSale,
    - getAll изменён на getActiveAll, который возвращает все активные продажи текущего менеджера
    - изменена реализация fillSale, т.к. в БД добавлены поля isActive и isConfirmed.

--- iSalary/iSalary/Sale_DB.cpp
+++ iSalary/iSalary/Sale_DB.cpp
@@ -14,19 +14,21 @@ Sale_DB::~Sale_DB(void) {
 void Sale_DB::init() {
     QSqlQuery query( _db );
     query.prepare( "CREATE TABLE  IF NOT EXISTS `" + TABLE_NAME + 
-		"` (`id` int NOT NULL PRIMARY KEY AUTO_INCREMENT, `manager_id` int NOT NULL,`product_id` int NOT NULL, `price` DOUBLE NOT NULL, `count` int NOT NULL)" );
+		"` (`id` int NOT NULL PRIMARY KEY AUTO_INCREMENT, `manager_id` int NOT NULL, `product_id` int NOT NULL, `price` DOUBLE NOT NULL, `count` int NOT NULL, `isActive` INT(1) NOT NULL, `isConfirmed` INT(1) NOT NULL)" );
     query.exec();
 }
 
-void Sale_DB::create( Sale sale ) {
+void Sale_DB::create( ActiveSale sale ) {
 
     QSqlQuery query( _db );
 	query.prepare( QString( "INSERT INTO " + TABLE_NAME + 
-		" (`manager_id`, `product_id`, `price`, `count`) VALUES(:manager_id, :product_id, :price, :count)" ) );
+		" (`manager_id`, `product_id`, `price`, `count`, `isActive`, `isConfirmed`) VALUES(:manager_id, :product_id, :price, :count, :isActive, :isConfirmed)" ) );
 	query.bindValue( ":manager_id", sale.getSaler().getId() );
 	query.bindValue( ":product_id",  sale.getProduct().getId() );
 	query.bindValue( ":price", sale.getCost() );
 	query.bindValue( ":count",  sale.getCount() );
+	query.bindValue( ":isActive", 1 );
+	query.bindValue( ":isConfirmed",  0 );
 
     bool isOk = query.exec();
     if ( !isOk ) {
@@ -35,28 +37,49 @@ void Sale_DB::create( Sale sale ) {
     }
 }
 
-Sale Sale_DB::read( const QSqlQuery * sqlQuery ){
-    Sale sale;
+ActiveSale Sale_DB::read( const QSqlQuery * sqlQuery ){
+    ActiveSale sale;
     fillSale( sale, sqlQuery );
     return sale;
 }
 
-void Sale_DB::fillSale( Sale & sale, const QSqlQuery * sqlQuery ) {
+void Sale_DB::fillSale( ActiveSale & sale, const QSqlQuery * sqlQuery ) {
 	sale.setId( sqlQuery->value( "id" ).value<int>() );
 	sale.setSalerId( sqlQuery->value( "manager_id" ).value<int>() );
 	sale.setProductId( sqlQuery->value( "product_id" ).value<int>() );
 	sale.setCost( sqlQuery->value( "price" ).value<double>() );
 	sale.setCount( sqlQuery->value( "count" ).value<int>() );
+	if ( sqlQuery->value( "isConfirmed" ).value<int>() == 1 ) {
+		sale.confirm();
+	} else {
+		sale.cancelConfirm();
+	}
 }
 
-QVector<Sale> Sale_DB::getAll( int manager_id ) {
+QVector<ActiveSale> Sale_DB::getActiveAll( int manager_id ) {
     QSqlQuery query( QString( "SELECT * FROM " ) + TABLE_NAME, _db );
-    QVector<Sale> sales;
+    QVector<ActiveSale> sales;
     while ( query.next() ) {
-        Sale sale = read( &query );
-		if ( sale.getSalerId() == manager_id ) {
-			sales.append( sale );
-		}
+        if ( query.value( "isActive" ).value<int>() == 1 ) {
+			ActiveSale sale = read( &query );
+			if ( sale.getSalerId() == manager_id ) {
+				sales.append( sale );
+			}
+		}	
     }
     return sales;
 }
+
+//QVector<ClosedSale> Sale_DB::getClosedAll( int manager_id ) {
+//    QSqlQuery query( QString( "SELECT * FROM " ) + TABLE_NAME, _db );
+//    QVector<ActiveSale> sales;
+//    while ( query.next() ) {
+//        if ( query.value( "isActive" ).value<int>() == 0 ) {
+//			ClosedSale sale = read( &query );
+//			if ( sale.getSalerId() == manager_id ) {
+//				sales.append( sale );
+//			}
+//		}	
+//    }
+//    return sales;
+//}
--- iSalary/iSalary/Sale_DB.h
+++ iSalary/iSalary/Sale_DB.h
@@ -2,7 +2,8 @@
 #include <QSqlDatabase>
 #include <QVariant>
 #include <QSqlResult>
-#include "Sale.h"
+#include "ActiveSale.h"
+#include "ClosedSale.h"
 #include <QVector>
 #include <QSqlError>
 
@@ -12,15 +13,13 @@ public:
 	Sale_DB( QSqlDatabase &db, QString table_name );
 	~Sale_DB(void);
 	void init();
-	void create(Sale sale);
-	void update(Sale sale);
-	void remove(int id);
-	void fillSale( Sale & sale, const QSqlQuery * sqlQuery );
-	QVector<Sale> getAll(int manager_id);
+	void create( ActiveSale sale );
+	void fillSale( ActiveSale & sale, const QSqlQuery * sqlQuery );
+	QVector<ActiveSale> getActiveAll( int manager_id );
 
 protected:
 	QString TABLE_NAME;
-	Sale read(const QSqlQuery * sqlQuery);
+	ActiveSale read(const QSqlQuery * sqlQuery);
 
 private:
 	QSqlDatabase _db;
