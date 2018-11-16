9d898e8ab0b5456a48007bf3257df371130ae247
Kurnosov Andrew <dryush97@gmail.com>
2018-11-16 12:51:39 +0400

    Установка даты найма

--- iSalary/iSalary/Employer.cpp
+++ iSalary/iSalary/Employer.cpp
@@ -10,11 +10,12 @@ Employer::Employer( AuthorizationModule* authorizationModule, IManagerRepository
 
 Manager& Employer::employ( Manager& manager){
     QString error;
-    
+
     if( this->managerValidator->isManagerValid(manager, &error)) {
         SignUpResult signUpRes = this->authorizationModule->signUp( manager.getLogin(), manager.getPassword(), MANAGER);
         if( signUpRes.success){
             manager.setUserInfo( signUpRes.user);
+            manager.setDateOfEmployment( QDate::currentDate());
             this->managerRepository->update( manager);
         } else {
             throw new QString( signUpRes.failReason);
