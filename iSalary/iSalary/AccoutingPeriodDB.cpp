b08c48689135a16be099a7499f06646f6e15d87d
Polikarpova <policarpova97@mail.ru>
2018-11-18 20:15:32 +0300

    Исправила проблему с созданием самого первого периода При создании таблицы для РП неверно задавались имена колонок

--- iSalary/iSalary/AccoutingPeriodDB.cpp
+++ iSalary/iSalary/AccoutingPeriodDB.cpp
@@ -13,7 +13,7 @@ void AccoutingPeriodDB::init() {
 	this->statusField = "status";
 
 	QSqlQuery query( *db );
-	query.prepare( "CREATE TABLE  IF NOT EXISTS `" + this->tableName + "` ( `id` int(11) NOT NULL AUTO_INCREMENT, `dataFrom` date NOT NULL, `dataTo` date DEFAULT NULL, `status` int(11) NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;" );
+	query.prepare( "CREATE TABLE  IF NOT EXISTS `" + this->tableName + "` ( `id` int(11) NOT NULL AUTO_INCREMENT, `dateFrom` date NOT NULL, `dateTo` date DEFAULT NULL, `status` int(11) NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=latin1;" );
 	this->execQuery( query);
 
 	this->initAccountingPeriod();
