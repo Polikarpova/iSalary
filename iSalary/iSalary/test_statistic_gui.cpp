214c60aa15f2afa390655b58925d09c1fe8210f2
DimaDontsov <Dimayahoo@mail.ru>
2019-01-10 18:28:52 +0400

    Обновил тестирование GUI страницы Продаж

--- iSalary/iSalary/test_statistic_gui.cpp
+++ iSalary/iSalary/test_statistic_gui.cpp
@@ -2,228 +2,372 @@
 
 Test_Statistic_GUI::Test_Statistic_GUI( QSqlDatabase *sql, StatisticPage *statisticPage ) {
 
-	this->statisticPage = statisticPage;
-	this->sql = sql;
-	user_DB = new UserDB( sql );
-	manager_DB = new ManagerDB( sql, user_DB );
-	product_DB = new Product_DB( sql, "products" );
-	product_DB->init();
-	sale_DB = new Sale_DB( sql, "sales");
-	sale_DB->init();
-	c = QTextCodec::codecForLocale();
+    this->statisticPage = statisticPage;
+    this->sql = sql;
+    user_DB = new UserDB( sql );
+    manager_DB = new ManagerDB( sql, user_DB );
+    product_DB = new Product_DB( sql, "products" );
+    product_DB->init();
+    sale_DB = new Sale_DB( sql, "sales" );
+    sale_DB->init();
+    c = QTextCodec::codecForLocale();
 }
 
-Test_Statistic_GUI::~Test_Statistic_GUI()
-{
-
-}
+Test_Statistic_GUI::~Test_Statistic_GUI() {}
 
 void Test_Statistic_GUI::removeEmployee( int id ) {
-	QSqlQuery query("DELETE FROM users WHERE id = " + QString::number( id ), *sql);
-	bool isOk = query.exec();
+    QSqlQuery query( "DELETE FROM users WHERE id = " + QString::number( id ), *sql );
+    bool isOk = query.exec();
 }
 
 void Test_Statistic_GUI::removeProduct( QString productName ) {
-	Product product = product_DB->findByName( productName );
-	QSqlQuery query("DELETE FROM products WHERE id = " + QString::number( product.getId() ), *sql);
-	bool isOk = query.exec();
+    Product product = product_DB->findByName( productName );
+    QSqlQuery query( "DELETE FROM products WHERE id = " + QString::number( product.getId() ), *sql );
+    bool isOk = query.exec();
 }
 
 Product Test_Statistic_GUI::createProduct( QString productName, double productCommission ) {
-	Product product;
-	product.setName( productName );
-	product.setCommission( productCommission );
-	product_DB->create( product );
-	return product;
-} 
-
-QString Test_Statistic_GUI::findElemInStatisticTable( int row, int col ){
-	return statisticPage->statisticTable->model()->data( statisticPage->statisticTable->model()->index( row, col ) ).toString();
+    Product product;
+    product.setName( productName );
+    product.setCommission( productCommission );
+    product_DB->create( product );
+    return product;
 }
 
-QString Test_Statistic_GUI::findElemInStatisticSalesTable( int row, int col ){
-	return statisticPage->statisticSalesTable->model()->data( statisticPage->statisticSalesTable->model()->index( row, col ) ).toString();
+QString Test_Statistic_GUI::findElemInStatisticTable( int row, int col ) {
+    return statisticPage->statisticTable->model()->data( statisticPage->statisticTable->model()->index( row, col ) ).toString();
+}
+
+QString Test_Statistic_GUI::findElemInStatisticSalesTable( int row, int col ) {
+    return statisticPage->statisticSalesTable->model()->data( statisticPage->statisticSalesTable->model()->index( row, col ) ).toString();
 }
 
 ActiveSale Test_Statistic_GUI::createSale( Product product, QDate saleDate, int manager_id ) {
-	ActiveSale sale;
-	sale.setConfirmDate( QDate::currentDate() );
-	sale.setSaleDate( saleDate );
-	sale.setCost( 100 );
-	sale.setCount( 3 );
-	sale.setProduct( product );
-	Manager saler;
-	saler.setId( manager_id );
-	sale.setSaler( saler );
-	sale_DB->create( sale );
-	sale = sale_DB->getActiveAll( manager_id ).last();
-	return sale;
+    ActiveSale sale;
+    sale.setConfirmDate( QDate::currentDate() );
+    sale.setSaleDate( saleDate );
+    sale.setCost( 100 );
+    sale.setCount( 3 );
+    sale.setProduct( product );
+    Manager saler;
+    saler.setId( manager_id );
+    sale.setSaler( saler );
+    sale_DB->create( sale );
+    sale = sale_DB->getActiveAll( manager_id ).last();
+    return sale;
 }
 
 void Test_Statistic_GUI::compareRowInStatisticTable( Manager manager, int row ) {
-	
-	QString exp_FIO = manager.getSecondName() + " " +  manager.getFirstName() + " " + manager.getThirdName();
 
-	QVector<ActiveSale>sales = sale_DB->getActiveAll( manager.getId() );
-
+    QString exp_FIO = manager.getSecondName() + " " + manager.getFirstName() + " " + manager.getThirdName();
+    
 	double currentSalary = 0;
-	double allSalary = 0;
-	double previousSalary = 0;
-	double exp_allCost = 0;
-	int exp_countSales = 0;
-	for ( int i = 0; i < sales.size(); i++ ) {
-		int salary = sales[i].getCount() * sales[i].getCost() * sales[i].getProductCommission() / 100;
-		if ( sales[i].getSaleDate() >= statisticPage->statisticMonth->date() ) {
-			currentSalary += salary;
-			exp_countSales++;
+    double allSalary = 0;
+    double previousSalary = 0;
+    double exp_allCost = 0;
+    int exp_countSales = 0;
+
+	QList<Manager> managers = manager_DB->getAll();
+
+	for ( int manager_num = 0; manager_num < managers.size(); manager_num++ ) {
+
+		QVector<ActiveSale> sales = sale_DB->getActiveAll( managers[ manager_num ].getId() );
+		for ( int i = 0; i < sales.size(); i++ ) {
+			
+			int salary = sales[i].getCount() * sales[i].getCost();
+			
+			if ( sales[i].getSaleDate() >= statisticPage->statisticMonth->date() ) {
+				
+				if ( managers[ manager_num ].getId() == manager.getId() ) {
+					
+					currentSalary += salary;
+					exp_countSales++;
+					exp_allCost += sales[i].getCount() * sales[i].getCost();
+				}
+				allSalary += salary;
+			} else if ( sales[i].getSaleDate() >= statisticPage->statisticMonth->date().addMonths( -1 ) && 
+						managers[ manager_num ].getId() == manager.getId() ) {
+				
+				previousSalary += salary;
+			}
 		}
-		else if ( sales[i].getSaleDate() >= statisticPage->statisticMonth->date().addMonths(-1) ) {
-			previousSalary += salary;
-		}
-		exp_allCost += sales[i].getCount() * sales[i].getCost();
-		allSalary += salary;
 	}
 
-	double exp_LET = 0, exp_OET = 0;
-	if ( previousSalary > 0 ) {
-		exp_LET = currentSalary / previousSalary;
-	}
-	if ( allSalary > 0 ) {
-		exp_OET = currentSalary / allSalary;
-	}
+    double exp_LET = 0, exp_OET = 0;
+    if ( previousSalary > 0 ) {
+        exp_LET = currentSalary / previousSalary;
+    }
+    if ( allSalary > 0 ) {
+        exp_OET = currentSalary / allSalary;
+    }
 
-	QString FIO = findElemInStatisticTable( row, 1 );
-	int countSales = findElemInStatisticTable( row, 2 ).toInt();
-	double allCost = findElemInStatisticTable( row, 3 ).toDouble();
-	double LET = findElemInStatisticTable( row, 4 ).toDouble();
-	double OET = findElemInStatisticTable( row, 5 ).toDouble();
+    QString FIO = findElemInStatisticTable( row, 1 );
+    int countSales = findElemInStatisticTable( row, 2 ).toInt();
+    double allCost = findElemInStatisticTable( row, 3 ).toDouble();
+    double LET = findElemInStatisticTable( row, 4 ).toDouble() / 100;
+    double OET = findElemInStatisticTable( row, 5 ).toDouble() / 100;
 
-	QCOMPARE( FIO, exp_FIO );
-	QCOMPARE( countSales, exp_countSales );
-	QCOMPARE( allCost, exp_allCost );
-	QCOMPARE( LET, exp_LET );
-	QCOMPARE( OET, exp_OET );
+    QCOMPARE( FIO, exp_FIO );
+    QCOMPARE( countSales, exp_countSales );
+    QCOMPARE( allCost, exp_allCost );
+    QCOMPARE( LET, exp_LET );
+    QCOMPARE( OET, exp_OET );
 }
 
 void Test_Statistic_GUI::compareRowInStatisticSalesTable( ActiveSale sale, int row ) {
-	
-	Manager manager = manager_DB->getById( sale.getSalerId() );
 
-	QString exp_FIO = manager.getSecondName() + " " +  manager.getFirstName() + " " + manager.getThirdName();
+    if ( sale.getSaleDate() < statisticPage->statisticMonth->date() ) {
 
-	QString FIO = findElemInStatisticSalesTable( row, 1 );
-	QString saleDate = findElemInStatisticSalesTable( row, 2 );
-	int countSale = findElemInStatisticSalesTable( row, 3 ).toInt();
-	QString productName = findElemInStatisticSalesTable( row, 4 );
-	double costSale = findElemInStatisticSalesTable( row, 5 ).toDouble();
-	double productPercent = findElemInStatisticSalesTable( row, 6 ).toDouble();
+        rowInStatisticSalesTableIsEmpty( row );
+    } else {
 
-	QCOMPARE( exp_FIO, FIO );
-	QCOMPARE( saleDate, sale.getSaleDate().toString( Qt::DateFormat::ISODate ) );
-	QCOMPARE( productName, sale.getProductName() );
-	QCOMPARE( countSale, sale.getCount() );
-	QCOMPARE( costSale, sale.getCost() * sale.getCount() );
-	QCOMPARE( productPercent, sale.getProductCommission() );
+        Manager manager = manager_DB->getById( sale.getSalerId() );
+
+        QString exp_FIO = manager.getSecondName() + " " + manager.getFirstName() + " " + manager.getThirdName();
+
+        QString FIO = findElemInStatisticSalesTable( row, 1 );
+        QString saleDate = findElemInStatisticSalesTable( row, 2 );
+        int countSale = findElemInStatisticSalesTable( row, 3 ).toInt();
+        QString productName = findElemInStatisticSalesTable( row, 4 );
+        double costSale = findElemInStatisticSalesTable( row, 5 ).toDouble();
+        double productPercent = findElemInStatisticSalesTable( row, 6 ).toDouble();
+
+        QCOMPARE( exp_FIO, FIO );
+        QCOMPARE( saleDate, sale.getSaleDate().toString( Qt::DateFormat::ISODate ) );
+        QCOMPARE( productName, sale.getProductName() );
+        QCOMPARE( countSale, sale.getCount() );
+        QCOMPARE( costSale, sale.getCost() * sale.getCount() );
+        QCOMPARE( productPercent, sale.getProductCommission() );
+    }
 }
 
-void Test_Statistic_GUI::statisticSalesTableIsEmpty() {
-	
-	QString FIO = findElemInStatisticSalesTable( 0, 1 );
-	QString saleDate = findElemInStatisticSalesTable( 0, 2 );
-	int countSale = findElemInStatisticSalesTable( 0, 3 ).toInt();
-	QString productName = findElemInStatisticSalesTable( 0, 4 );
-	double costSale = findElemInStatisticSalesTable( 0, 5 ).toDouble();
-	QString productPercent = findElemInStatisticSalesTable( 0, 6 );
-	
-	QCOMPARE( FIO, QString( "" ) );
-	QCOMPARE( saleDate, QString( "" ) );
-	QCOMPARE( productName, QString( "" ) );
-	QCOMPARE( countSale, 0 );
-	QCOMPARE( costSale, 0.0 );
-	QCOMPARE( productPercent, QString( "" ) );
+void Test_Statistic_GUI::rowInStatisticSalesTableIsEmpty( int row ) {
+
+    QString FIO = findElemInStatisticSalesTable( row, 1 );
+    QString saleDate = findElemInStatisticSalesTable( row, 2 );
+    int countSale = findElemInStatisticSalesTable( row, 3 ).toInt();
+    QString productName = findElemInStatisticSalesTable( row, 4 );
+    double costSale = findElemInStatisticSalesTable( row, 5 ).toDouble();
+    QString productPercent = findElemInStatisticSalesTable( row, 6 );
+
+    QCOMPARE( FIO, QString( "" ) );
+    QCOMPARE( saleDate, QString( "" ) );
+    QCOMPARE( productName, QString( "" ) );
+    QCOMPARE( countSale, 0 );
+    QCOMPARE( costSale, 0.0 );
+    QCOMPARE( productPercent, QString( "" ) );
 }
 
-void Test_Statistic_GUI::managerStatisticIsEmpty( Manager manager, int row) {
-	
-	QString exp_FIO = manager.getSecondName() + " " +  manager.getFirstName() + " " + manager.getThirdName();
-	
-	QString FIO = findElemInStatisticTable( row, 1 );
-	int countSales = findElemInStatisticTable( row, 2 ).toInt();
-	double costSale = findElemInStatisticTable( row, 3 ).toDouble();
-	double LET = findElemInStatisticTable( row, 4 ).toDouble();
-	double OET = findElemInStatisticTable( row, 5 ).toDouble();
-	
-	QCOMPARE( FIO, exp_FIO );
-	QCOMPARE( countSales, 0 );
-	QCOMPARE( costSale, 0.0 );
-	QCOMPARE( LET, 0.0 );
-	QCOMPARE( LET, 0.0 );
+void Test_Statistic_GUI::managerStatisticIsEmpty( Manager manager, int row ) {
+
+    QString exp_FIO = manager.getSecondName() + " " + manager.getFirstName() + " " + manager.getThirdName();
+
+    QString FIO = findElemInStatisticTable( row, 1 );
+    int countSales = findElemInStatisticTable( row, 2 ).toInt();
+    double costSale = findElemInStatisticTable( row, 3 ).toDouble();
+    double LET = findElemInStatisticTable( row, 4 ).toDouble();
+    double OET = findElemInStatisticTable( row, 5 ).toDouble();
+
+    QCOMPARE( FIO, exp_FIO );
+    QCOMPARE( countSales, 0 );
+    QCOMPARE( costSale, 0.0 );
+    QCOMPARE( LET, 0.0 );
+    QCOMPARE( LET, 0.0 );
 }
 
 
 
 void Test_Statistic_GUI::showCountManagerSales() {
 
-	statisticPage->statisticMonth->setDate( QDate( 2019, 01, 01 ) );
+    statisticPage->statisticMonth->setDate( QDate( 2019, 01, 01 ) );
 
-	User user;
-	user = user_DB->insert( user, UserType::MANAGER );
-	int manager_id = user.getId();
+    User user;
+    user = user_DB->insert( user, UserType::MANAGER );
+    int manager_id = user.getId();
 
-	QString firstName = "Ivan";
-	QString secondName = "Ivanov";
-	QString thirdName = "Ivanovich";
+    QString firstName = "Ivan";
+    QString secondName = "Ivanov";
+    QString thirdName = "Ivanovich";
 
-	Manager manager;
-	manager.setId( manager_id );
-	manager.setFirstName( firstName );
-	manager.setSecondName( secondName );
-	manager.setThirdName( thirdName );
+    Manager manager;
+    manager.setId( manager_id );
+    manager.setFirstName( firstName );
+    manager.setSecondName( secondName );
+    manager.setThirdName( thirdName );
 
-	manager_DB->update( manager );
+    manager_DB->update( manager );
 
-	Product first_product = createProduct( "ABC", 15.3 );
-	Product second_product = createProduct( "AAA", 35 );
-	ActiveSale first_sale = createSale( first_product, QDate( 2019, 01, 01 ), manager_id );
-	ActiveSale second_sale = createSale( second_product, QDate( 2018, 12, 01 ), manager_id );
+    Product first_product = createProduct( "ABC", 15.3 );
+    Product second_product = createProduct( "AAA", 35 );
+    ActiveSale first_sale = createSale( first_product, QDate( 2019, 01, 01 ), manager_id );
+    ActiveSale second_sale = createSale( second_product, QDate( 2018, 12, 01 ), manager_id );
 
-	statisticPage->refreshPage();
+    statisticPage->refreshPage();
 
-	managerStatisticIsEmpty( manager, 0);
-	statisticSalesTableIsEmpty();
+    managerStatisticIsEmpty( manager, 0 );
+    rowInStatisticSalesTableIsEmpty( 0 );
 
-	sale_DB->confirmSale( first_sale.getId() );
-	sale_DB->confirmSale( second_sale.getId() );
+    sale_DB->confirmSale( first_sale.getId() );
+    sale_DB->confirmSale( second_sale.getId() );
 
-	statisticPage->refreshPage();
+    statisticPage->refreshPage();
 
-	compareRowInStatisticTable( manager, 0);
-	compareRowInStatisticSalesTable( first_sale, 0 );
-	compareRowInStatisticSalesTable( second_sale, 1 );
-
-	QVector<ActiveSale> sales = sale_DB->getActiveAll( manager_id );
-
-	sale_DB->remove( sales[0].getId() );
-	sale_DB->remove( sales[1].getId() );
-	removeProduct( first_product.getName() );
-	removeProduct( second_product.getName() );
-	removeEmployee( manager_id );
-
-	statisticPage->refreshPage();
+    compareRowInStatisticTable( manager, 0 );
+    compareRowInStatisticSalesTable( first_sale, 0 );
+    compareRowInStatisticSalesTable( second_sale, 1 );
 }
 
 void Test_Statistic_GUI::showSales() {
 
+    statisticPage->statisticMonth->setDate( QDate( 2019, 01, 01 ) );
+
+    User user;
+    user = user_DB->insert( user, UserType::MANAGER );
+    int manager_id = user.getId();
+
+    QString firstName = "Ivan";
+    QString secondName = "Ivanov";
+    QString thirdName = "Ivanovich";
+
+    Manager manager;
+    manager.setId( manager_id );
+    manager.setFirstName( firstName );
+    manager.setSecondName( secondName );
+    manager.setThirdName( thirdName );
+
+    manager_DB->update( manager );
+
+    Product first_product = createProduct( "ABC", 15.3 );
+    Product second_product = createProduct( "AAA", 35 );
+    ActiveSale first_sale = createSale( first_product, QDate( 2019, 01, 01 ), manager_id );
+    ActiveSale second_sale = createSale( second_product, QDate( 2018, 12, 01 ), manager_id );
+
+    sale_DB->confirmSale( first_sale.getId() );
+    sale_DB->confirmSale( second_sale.getId() );
+
+    statisticPage->refreshPage();
+
+    compareRowInStatisticSalesTable( first_sale, 0 );
 }
 
 void Test_Statistic_GUI::showCalendar() {
-
+	//QTextCharFormat x = statisticPage->statisticCalendar->dateTextFormat( QDate(2019, 01, 01) ).background().brush().color();
 }
 
 void Test_Statistic_GUI::showSalesInPreviousMonths() {
 
+    statisticPage->statisticMonth->setDate( QDate( 2018, 12, 01 ) );
+
+    User user;
+    user = user_DB->insert( user, UserType::MANAGER );
+    int manager_id = user.getId();
+
+    QString firstName = "Ivan";
+    QString secondName = "Ivanov";
+    QString thirdName = "Ivanovich";
+
+    Manager manager;
+    manager.setId( manager_id );
+    manager.setFirstName( firstName );
+    manager.setSecondName( secondName );
+    manager.setThirdName( thirdName );
+
+    manager_DB->update( manager );
+
+    Product first_product = createProduct( "ABC", 15.3 );
+    Product second_product = createProduct( "AAA", 35 );
+    ActiveSale first_sale = createSale( first_product, QDate( 2019, 01, 01 ), manager_id );
+    ActiveSale second_sale = createSale( second_product, QDate( 2018, 12, 01 ), manager_id );
+
+    sale_DB->confirmSale( first_sale.getId() );
+    sale_DB->confirmSale( second_sale.getId() );
+
+    statisticPage->refreshPage();
+
+    compareRowInStatisticSalesTable( second_sale, 0 );
 }
 
 void Test_Statistic_GUI::showAllManagersSales() {
+	
+	statisticPage->statisticMonth->setDate( QDate( 2019, 01, 01 ) );
 
+	User first_user;
+    first_user = user_DB->insert( first_user, UserType::MANAGER );
+    int first_manager_id = first_user.getId();
+
+    QString firstName = "Ivan";
+    QString secondName = "Ivanov";
+    QString thirdName = "Ivanovich";
+    QString first_exp_FIO = secondName + " " + firstName + " " + thirdName;
+
+    Manager manager;
+    manager.setId( first_manager_id );
+    manager.setFirstName( firstName );
+    manager.setSecondName( secondName );
+    manager.setThirdName( thirdName );
+
+    manager_DB->update( manager );
+
+    Product first_product = createProduct( "ABC", 15.3 );
+    Product second_product = createProduct( "AAA", 35 );
+    ActiveSale first_sale = createSale( first_product, QDate( 2019, 01, 01 ), first_manager_id );
+    sale_DB->confirmSale( first_sale.getId() );
+
+    User second_user;
+    second_user = user_DB->insert( second_user, UserType::MANAGER );
+    int second_manager_id = second_user.getId();
+
+    firstName = "Dima";
+    secondName = "Sokolov";
+    thirdName = "Ivanovich";
+    QString second_exp_FIO = secondName + " " + firstName + " " + thirdName;
+
+    manager.setId( second_manager_id );
+    manager.setFirstName( firstName );
+    manager.setSecondName( secondName );
+    manager.setThirdName( thirdName );
+
+    manager_DB->update( manager );
+
+    ActiveSale second_sale = createSale( second_product, QDate( 2019, 01, 01 ), second_manager_id );
+	sale_DB->confirmSale( second_sale.getId() );
+
+    statisticPage->refreshPage();
+
+	statisticPage->statisticTable->setFocus();
+    QModelIndex newIndex = statisticPage->statisticTable->model()->index( 0, 1 );
+    statisticPage->statisticTable->setCurrentIndex( newIndex );
+    statisticPage->statisticTable->clicked( newIndex );
+
+	compareRowInStatisticSalesTable( first_sale, 0 );
+
+	statisticPage->statisticTable->setFocus();
+    newIndex = statisticPage->statisticTable->model()->index( 1, 1 );
+    statisticPage->statisticTable->setCurrentIndex( newIndex );
+    statisticPage->statisticTable->clicked( newIndex );
+
+	compareRowInStatisticSalesTable( second_sale, 0 );
+}
+
+void Test_Statistic_GUI::cleanup() {
+	
+	QList<Manager> managers = manager_DB->getAll();
+	for ( int manager_num = 0; manager_num < managers.size(); manager_num++ ) {
+		
+		int manager_id = managers[ manager_num ].getId();
+		QVector<ActiveSale> sales = sale_DB->getActiveAll( manager_id );
+		for ( int sale_num = 0; sale_num < sales.size(); sale_num++ ) {
+			
+			sale_DB->remove( sales[ sale_num ].getId() );
+		}
+		removeEmployee( manager_id );
+	}
+	
+	QVector<Product> products = product_DB->getAll();
+    for ( int product_num = 0; product_num < products.size(); product_num++ ) {
+		
+		removeProduct( products[ product_num ].getName() );
+	}
+
+    statisticPage->refreshPage();
 }
--- iSalary/iSalary/test_statistic_gui.h
+++ iSalary/iSalary/test_statistic_gui.h
@@ -1,47 +1,49 @@
 #ifndef TEST_STATISTIC_GUI_H
 #define TEST_STATISTIC_GUI_H
 
-#include <QObject>
-#include <qtest.h>
+#include "Product_DB.h"
 #include "QTextCodec"
 #include "StatisticPage.h"
+#include <QObject>
 #include <QSqlDatabase>
-#include "Product_DB.h"
+#include <qtest.h>
 
-class Test_Statistic_GUI : public QObject
-{
-	Q_OBJECT
-	friend class StatisticPage;
-public:
-	Test_Statistic_GUI( QSqlDatabase *sql, StatisticPage *statisticPage );
-	~Test_Statistic_GUI();
+class Test_Statistic_GUI : public QObject {
+    Q_OBJECT
+    friend class StatisticPage;
 
-private:
-	StatisticPage *statisticPage;
-	Product_DB *product_DB;
-	UserDB *user_DB;
-	ManagerDB *manager_DB;
-	Sale_DB *sale_DB;
-	QSqlDatabase *sql;
-	QTextCodec * c;
+  public:
+    Test_Statistic_GUI( QSqlDatabase *sql, StatisticPage *statisticPage );
+    ~Test_Statistic_GUI();
 
-	void removeProduct( QString productName );
-	Product createProduct( QString productName, double productCommission );
-	QString findElemInStatisticTable( int row, int col );
-	QString findElemInStatisticSalesTable( int row, int col );
-	ActiveSale createSale( Product product, QDate saleDate, int manaher_id );
-	void removeEmployee( int id );
-	void compareRowInStatisticTable( Manager manager, int row );
-	void compareRowInStatisticSalesTable( ActiveSale sale, int row );
-	void statisticSalesTableIsEmpty();
-	void managerStatisticIsEmpty( Manager manager, int row);
+  private:
+    StatisticPage *statisticPage;
+    Product_DB *product_DB;
+    UserDB *user_DB;
+    ManagerDB *manager_DB;
+    Sale_DB *sale_DB;
+    QSqlDatabase *sql;
+    QTextCodec *c;
 
-private slots:
-	void showCountManagerSales();
-	void showSales();
-	void showCalendar();
-	void showSalesInPreviousMonths();
-	void showAllManagersSales();
+    void removeProduct( QString productName );
+    Product createProduct( QString productName, double productCommission );
+    QString findElemInStatisticTable( int row, int col );
+    QString findElemInStatisticSalesTable( int row, int col );
+    ActiveSale createSale( Product product, QDate saleDate, int manaher_id );
+    void removeEmployee( int id );
+    void compareRowInStatisticTable( Manager manager, int row );
+    void compareRowInStatisticSalesTable( ActiveSale sale, int row );
+    void rowInStatisticSalesTableIsEmpty( int row );
+    void managerStatisticIsEmpty( Manager manager, int row );
+
+  private slots:
+    void showCountManagerSales();
+    void showSales();
+    void showCalendar();
+    void showSalesInPreviousMonths();
+    void showAllManagersSales();
+	//void init();
+	void cleanup();
 };
 
 #endif // TEST_STATISTIC_GUI_H
